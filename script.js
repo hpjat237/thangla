41a7e5d7496ba7e8ad775822535cb33db3da4c04
hpjat237 <falai2372004@gmail.com>
2025-04-06 11:30:55 +0700

    update slide bar, footer

--- script.js
+++ script.js
@@ -2,12 +2,18 @@
 const sidebarItems = document.querySelectorAll('.sidebar ul li');
 
 sidebarItems.forEach(item => {
-    item.addEventListener('click', function() {
+    item.addEventListener('click', function(event) {
+        // Prevent dropdown from closing if you click on a dropdown item
+        if (event.target.tagName === 'A') {
+            window.location.href = event.target.href; // Navigate to the new HTML page
+        }
+
         const dropdown = item.querySelector('ul');
         
         if (dropdown) {
             // Toggle class to show or hide dropdown
             dropdown.classList.toggle('active');
+            item.classList.toggle('active'); // Toggle arrow rotation
         }
     });
 });
