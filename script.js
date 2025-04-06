d6829d9895de625cc65f1f2e8fce9c5d52f10b42
hpjat237 <falai2372004@gmail.com>
2025-04-06 11:35:21 +0700

    update ux

--- script.js
+++ script.js
@@ -4,12 +4,14 @@ const sidebarItems = document.querySelectorAll('.sidebar ul li');
 sidebarItems.forEach(item => {
     item.addEventListener('click', function(event) {
         // Prevent dropdown from closing if you click on a dropdown item
+        const dropdown = item.querySelector('ul');
+        
+        // If the clicked target is an anchor tag (li > a), navigate to the corresponding page
         if (event.target.tagName === 'A') {
+            event.stopPropagation(); // Prevent closing the dropdown
             window.location.href = event.target.href; // Navigate to the new HTML page
         }
 
-        const dropdown = item.querySelector('ul');
-        
         if (dropdown) {
             // Toggle class to show or hide dropdown
             dropdown.classList.toggle('active');
