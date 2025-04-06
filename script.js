04ea83a012841cca0d9605a44fe597ce0442226f
hpjat237 <falai2372004@gmail.com>
2025-04-06 10:45:53 +0700

    update slide bar

--- script.js
+++ script.js
@@ -6,7 +6,56 @@ links.forEach(link => {
     link.addEventListener('click', function(e) {
         e.preventDefault();
         const course = link.textContent;
-        contentArea.innerHTML = `<h2>${course}</h2><p>This is the content for the course
+        contentArea.innerHTML = `
+        <h2>${course}</h2>
+        <h2>${course}</h2>
+        <h2>${course}</h2>
+        <h2>${course}</h2>
+        <h2>${course}</h2>
+        <h2>${course}</h2>
+        <h2>${course}</h2>
+        <h2>${course}</h2>
+                <h2>${course}</h2>
+        <h2>${course}</h2>
+        <h2>${course}</h2>
+        <h2>${course}</h2>
+        <h2>${course}</h2>
+        <h2>${course}</h2>
+        <h2>${course}</h2>
+        <h2>${course}</h2>
+                <h2>${course}</h2>
+        <h2>${course}</h2>
+        <h2>${course}</h2>
+        <h2>${course}</h2>
+        <h2>${course}</h2>
+        <h2>${course}</h2>
+        <h2>${course}</h2>
+        <h2>${course}</h2>
+                <h2>${course}</h2>
+        <h2>${course}</h2>
+        <h2>${course}</h2>
+        <h2>${course}</h2>
+        <h2>${course}</h2>
+        <h2>${course}</h2>
+        <h2>${course}</h2>
+        <h2>${course}</h2>
+                <h2>${course}</h2>
+        <h2>${course}</h2>
+        <h2>${course}</h2>
+        <h2>${course}</h2>
+        <h2>${course}</h2>
+        <h2>${course}</h2>
+        <h2>${course}</h2>
+        <h2>${course}</h2>
+                <h2>${course}</h2>
+        <h2>${course}</h2>
+        <h2>${course}</h2>
+        <h2>${course}</h2>
+        <h2>${course}</h2>
+        <h2>${course}</h2>
+        <h2>${course}</h2>
+        <h2>${course}</h2>
+        <p>This is the content for the course
         This is the content for the course
         This is the content for the course
         This is the content for the course
